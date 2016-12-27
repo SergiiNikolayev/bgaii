a909f75be4a00b58b86b9320e16ded324d974fbd
Sergii Nikolayev <nikolayev.sergii@gmail.com>
2016-12-27 12:38:09 +0200

    on example of 17 chapter we can see new look
    JSON work and add info to main page of chapter :heavy_plus_sign: we've
    got nice filter of contnet of main page, so it would easy to serch for
    needed shloka

--- jsScripts/myFunctions.js
+++ jsScripts/myFunctions.js
@@ -7,6 +7,52 @@ function scrollMeUp(){
 
 /** JSON file*/
 var jsonBg = {
+    chapter17:[
+        {
+            id: 0,
+            /** empty block for 0 element*/
+            shloka: "ss",
+            posl: "pp",
+            tran: "tt",
+            comm: "cc"
+        },
+        {
+            id: 1,
+            shloka: "арджуна увча  йе стра-видхим утсджйайаджанте раддхайнвиттеш нишх ту к кшасаттвам хо раджас тама",
+            posl: "word translation 17.1",
+            tran: "Арджуна спросил: О Кришна, каково положение тех, кто не следует предписаниям шастр, но выдумывает собственные способы поклонения? Под влиянием какой гуны — благости, страсти или невежества — они находятся?",
+            comm: "purpot 17.1"
+        },
+
+        {
+            id: 2,
+            shloka: "рӣ-бхагавн увча три-видх бхавати раддх дехин с свабхва-дж сттвикӣ рджасӣ чаива тмасӣ чети т у",
+            posl: "word translation 17.2",
+            tran: "Верховный Господь сказал: В зависимости от гун материальной природы, под влиянием которых оказалась воплощенная душа, ее вера может быть трех видов — в благости, страсти или невежестве. Услышь же от Меня об этом.",
+            comm: "purpot 17.2"
+        },
+        {
+            id: 3,
+            shloka: "shloka text 17.3",
+            posl: "word translation 17.3",
+            tran: "russ trans 17.3",
+            comm: "purpot 17.3"
+        },
+        {
+            id: 4,
+            shloka: "shloka text 17.4",
+            posl: "word translation 17.4",
+            tran: "russ trans 17.4",
+            comm: "purpot 17.4"
+        },
+        {
+            id: 5,
+            shloka: "shloka text 17.5",
+            posl: "word translation 17.5",
+            tran: "russ trans 17.5",
+            comm: "purpot 17.5"
+        }
+    ],
     chapter18:[
         {
             id: 0,
